f46d6089566f15d379bff5cd9bf473a39e47ff72
Andrey <vladikin@gmail.com>
2017-10-09 22:25:45 +0300

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,57 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-const dirPath = path.resolve(__dirname, 'dist');
+const srcPath = path.resolve(__dirname, 'src');
+const distPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-	entry : './src/app.js',
-	output : {
-		path : dirPath, 
-		filename : 'app.bundle.js'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: dirPath,
-		port: 3000,
-		hot: true
+    context: srcPath,
+    entry: './app.js',
+    output: {
+        path: distPath,
+        filename: 'app.bundle.js'
     },
-	module: {
-		loaders: [
-		{
-			test: /\.html$/,
-			loader: 'html-loader'
-		}
-		],
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/, 
-				loader: "babel-loader"
-			},
-			{
-				test: /\.less$/,
-				use: [{
-					loader: "style-loader" // creates style nodes from JS strings
-				}, {
-					loader: "css-loader" // translates CSS into CommonJS
-				}, {
-					loader: "less-loader" // compiles Less to CSS
-				}]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'My App',
-			template: 'src/index.html',
-			files: {
-				"css": [ "test.css" ]
-			}
-		}),
-		new webpack.HotModuleReplacementPlugin()
-	]
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: distPath,
+        port: 3000,
+        hot: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {test: /\.png$/, loader: 'file-loader'}
+        ],
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            favicon: './favicon.ico',
+            title: 'My App',
+            files: {
+                "css": ["test.css"]
+            }
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
