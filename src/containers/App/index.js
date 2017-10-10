1f5c56b92350a008ca7eee4890366d5142429703
Andrey <vladikin@gmail.com>
2017-10-10 22:39:25 +0300

    eslint rules. LF mode

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<h1>React App</h1>
-		);
-	}
+class App extends React.Component {
+    render() {
+        return (
+            <h1>React App</h1>
+        );
+    }
 }
+
+export default App;
