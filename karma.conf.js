38435ef6dd8e2b48153fbbf205382124782487a7
Andrey <vladikin@gmail.com>
2017-10-11 00:29:01 +0300

    Karma mocha nyan

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        files: [
+            {
+                pattern: 'src/**/test/*.js',
+                watched: true
+            }
+        ],
+
+        frameworks: ['mocha'],
+        browsers: ['PhantomJS'],
+
+        preprocessors: {
+            'src/**/test/*.js': ['webpack', 'sourcemap']
+        },
+
+        reporters: ['nyan'],
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            stats: 'errors-only'
+        },
+
+        plugins: [
+            require("karma-webpack"),
+            require("karma-mocha"),
+            require("karma-phantomjs-launcher"),
+            require("karma-sourcemap-loader"),
+            require("karma-nyan-reporter")
+        ],
+
+        nyanReporter: {
+            suppressErrorHighlighting: true,
+        },
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false,
+    });
+};
